4eb9de06f0a67c22c98f3479a7cfe5ece14f361a
Elina Eriksson Hult <elinaerikssonhult@gmail.com>
2024-10-18 23:20:09 +0200

    Update - added styling elements, different styling as a default and changing colors once form is filled in. Added content in FavoriteInstrument component to apply the change of colors elements in css file

--- src/components/FavoriteInstrument.jsx
+++ src/components/FavoriteInstrument.jsx
@@ -4,7 +4,13 @@ export const FavoriteInstrument = ({ formData, handleChange }) => {
       <h2>1.</h2>
       <h3>Choose your favorite instrument</h3>
       <div className="radio-group">
-        <label className="radio-button" htmlFor="guitar">
+
+      {/* // Updated the label to dynamically apply the "checked" class when the user selects "Guitar".
+      // This allows the background or other styles to change based on the selected option. */}
+      <label
+          className={`radio-button ${formData.fav_instrument === "Guitar" ? "checked" : ""}`} 
+          htmlFor="guitar"
+        >
           <input
             type="radio"
             id="guitar"
@@ -16,7 +22,13 @@ export const FavoriteInstrument = ({ formData, handleChange }) => {
           Guitar
           <span className="checkmark"></span>
         </label>
-        <label className="radio-button" htmlFor="piano">
+
+      {/* // Updated the label to dynamically apply the "checked" class when the user selects "Piano".
+      // This allows the background or other styles to change based on the selected option. */}
+        <label
+          className={`radio-button ${formData.fav_instrument === "Piano" ? "checked" : ""}`} 
+          htmlFor="piano"
+        >
           <input
             type="radio"
             id="piano"
@@ -28,7 +40,13 @@ export const FavoriteInstrument = ({ formData, handleChange }) => {
           Piano
           <span className="checkmark"></span>
         </label>
-        <label className="radio-button" htmlFor="drums">
+
+      {/* // Updated the label to dynamically apply the "checked" class when the user selects "Drums".
+      // This allows the background or other styles to change based on the selected option. */}       
+        <label
+          className={`radio-button ${formData.fav_instrument === "Drums" ? "checked" : ""}`} 
+          htmlFor="drums"
+        >
           <input
             type="radio"
             id="drums"
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,7 +2,7 @@ export const Results = ({ artist, fav_genre, fav_instrument }) => {
   return (
     <div>
       <p>
-        Your seem to like {artist}, {fav_genre} and {fav_instrument}
+        You seem to like {artist}, {fav_genre} and {fav_instrument}
       </p>
     </div>
   );
